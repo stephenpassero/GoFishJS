ea2e402cacdc5445c3db5c67bd9fd4ec32e3c12f
stephenpassero <stephen.passero@gmail.com>
2019-06-12 12:09:39 -0400

    Make highlight on selected card re-render everything

--- src/GameView.js
+++ src/GameView.js
@@ -3,50 +3,53 @@ class GameView {
   constructor(game) {
     this._game = game
     this._humanPlayerName = this._game.playerName()
-    this._cardSelected = ''
-    this._opponentSelected = ''
+    this._selectedCard = ''
+    this._selectedOpponent = ''
+    this._container = ''
   }
 
-  cardClicked(card, cards) {
-    cards.forEach(card => card.classList.remove('selected'))
-    card.classList.add('selected')
-    const rank = card.name
-    this._cardSelected = rank
-    console.log(this._cardSelected)
+  cardClicked(card) {
+    this._selectedCard = card.name
+    this._container.innerHTML = ''
+    this.render(this._container)
   }
 
-  opponentClicked(opponentDiv, allDivs) {
-    allDivs.forEach(div => div.classList.remove('selected'))
-    opponentDiv.classList.add('selected')
-    const name = opponentDiv.id
-    this._opponentSelected = name
-    console.log(this._opponentSelected)
+  opponentClicked(opponentDiv) {
+    this._selectedOpponent = opponentDiv.id
+    this._container.innerHTML = ''
+    this.render(this._container)
   }
 
   getCardImages(playerName) {
     const cards = this._game.findPlayer(playerName).cards()
-    return cards.map(card => `<img class="card" name="${card.rank()}" src="public/img/cards/${card.imagePath()}.png" />`)
+    return cards.map((card) => {
+      if (card.rank() === this._selectedCard) {
+        return `<img class="card selected" name="${card.rank()}" src="public/img/cards/${card.imagePath()}.png" />`
+      }
+      return `<img class="card" name="${card.rank()}" src="public/img/cards/${card.imagePath()}.png" />`
+    })
   }
 
   addHighlightOnClick() {
     const cardsImages = document.querySelectorAll('.card')
     for (const card of cardsImages) {
-      card.onclick = this.cardClicked.bind(this, card, cardsImages)
+      card.onclick = this.cardClicked.bind(this, card)
     }
     const opponentsDiv = document.querySelectorAll('.opponent')
     for (const opponentDiv of opponentsDiv) {
-      opponentDiv.onclick = this.opponentClicked.bind(this, opponentDiv, opponentsDiv)
+      opponentDiv.onclick = this.opponentClicked.bind(this, opponentDiv)
     }
   }
 
   // Clean up this method
   render(container) {
+    this._container = container
     // Re-render this when the user picks a card and an opponent
     const div = document.createElement('div')
     let opponentView
     const botHTML = this._game.botNames().map((name) => {
       const player = this._game.findPlayer(name)
-      opponentView = new OpponentView(name, player.cards(), player.pairs())
+      opponentView = new OpponentView(name, player.cards(), player.pairs(), this._selectedOpponent)
       return opponentView.render(container)
     })
     const cardImages = this.getCardImages(this._humanPlayerName)
--- src/OpponentView.js
+++ src/OpponentView.js
@@ -1,19 +1,32 @@
 class OpponentView {
-  constructor(name, cards, pairs) {
+  constructor(name, cards, pairs, selectedOpponent) {
     this._name = name
     this._cards = cards
     this._pairs = pairs
+    this._selectedOpponent = selectedOpponent
+  }
+
+  getSelected() {
+    const opponentMarkup = `<h3>${this._name}</h3>
+    ${this._cards.map(card => '<img class="cardBack" src="public/img/cards/backs_red.png"/>').join('')}`
+    if (this._selectedOpponent === this._name) {
+      return `
+      <div class="opponent selected" id="${this._name}">
+        ${opponentMarkup}
+      </div>
+      `
+    }
+    return `
+    <div class="opponent" id="${this._name}">
+      ${opponentMarkup}
+    </div>
+    `
   }
 
   render(container) {
     const div = document.createElement('div')
     // Eventually render pairs here - Don't forget to write tests
-    const opponent = `
-    <div class="opponent" id="${this._name}">
-      <h3>${this._name}</h3>
-      ${this._cards.map(card => '<img class="cardBack" src="public/img/cards/backs_red.png"/>').join('')}
-    </div>
-    `
+    const opponent = this.getSelected()
     div.innerHTML = opponent
     container.appendChild(div)
   }

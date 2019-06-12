eafcd5fcf51f4862eb036531f401cedb079ed09c
stephenpassero <stephen.passero@gmail.com>
2019-06-12 10:01:10 -0400

    Add highlighting on click on cards and write test for GameView

--- spec/GameViewSpec.js
+++ spec/GameViewSpec.js
@@ -1,16 +1,34 @@
 /* eslint no-undef: 0 */
 describe('GameView', () => {
+  beforeEach(() => {
+    game = new Game('A Cool Person', 4)
+    game.startGame()
+    gameView = new GameView(game)
+    container = document.createElement('div')
+    document.body.appendChild(container)
+    gameView.render(container)
+  })
+
+  afterEach(() => {
+    container.remove()
+  })
+
   describe('#render', () => {
     it('shows the player and the correct number of bots', () => {
-      const game = new Game('A Cool Person', 5)
-      game.startGame()
-      const gameView = new GameView(game)
-      const container = document.createElement('div')
-      document.body.appendChild(container)
-      gameView.render(container)
       expect(document.body.querySelector('h2').textContent).toEqual('A Cool Person')
-      expect(document.body.querySelectorAll('h3').length).toEqual(4)
-      container.remove()
+      expect(document.body.querySelectorAll('h3').length).toEqual(3)
+    })
+
+    it('shows the correct number of cards', () => {
+      const cardsOnScreen = document.querySelectorAll('img').length
+      // 4 players with 5 cards each: 4 * 5 = 20
+      expect(cardsOnScreen).toEqual(20)
+    })
+
+    it('highlights a card when the card is clicked', () => {
+      const card = document.querySelector('.card')
+      card.click()
+      expect(card.classList).toContain('selected')
     })
   })
 })
--- src/GameView.js
+++ src/GameView.js
@@ -7,21 +7,30 @@ class GameView {
     this._opponentSelected = ''
   }
 
-  cardClicked(cardRank) {
-    this._cardSelected = cardRank
+  cardClicked(card, cards) {
+    cards.forEach(card => card.classList.remove('selected'))
+    card.classList.add('selected')
+    const rank = card.name
+    this._cardSelected = rank
     console.log(this._cardSelected)
   }
 
-  opponentClicked(opponentName) {
-    this._opponentSelected = opponentName
+  opponentClicked(opponentDiv, allDivs) {
+    allDivs.forEach(div => div.classList.remove('selected'))
+    opponentDiv.classList.add('selected')
+    const name = opponentDiv.id
+    this._opponentSelected = name
     console.log(this._opponentSelected)
   }
 
   // Clean up this method
   render(container) {
+    // Re-render this when the user picks a card and an opponent
     const cards = this._game.findPlayer(this._humanPlayerName).cards()
     const div = document.createElement('div')
     const botHTML = this._game.botNames().map((name) => {
+      // This won't always work
+      // Refactor this into a seperate view or something
       return `
       <div class="opponent" id="${name}">
         <h3>${name}</h3>
@@ -32,20 +41,20 @@ class GameView {
     const cardImages = cards.map(card => `<img class="card" name="${card.rank()}" src="public/img/cards/${card.imagePath()}.png" />`)
     const gameView = `
       ${botHTML.join('')}
-      <div class="humanCards">
+      <div class="human">
         <h2>${this._humanPlayerName}</h2>
         ${cardImages.join('')}
       </div>
     `
     div.innerHTML = gameView
     container.appendChild(div)
-    const cardsImage = document.querySelectorAll('.card')
-    for (const card of cardsImage) {
-      card.onclick = this.cardClicked.bind(this, card.name)
+    const cardsImages = document.querySelectorAll('.card')
+    for (const card of cardsImages) {
+      card.onclick = this.cardClicked.bind(this, card, cardsImages)
     }
     const opponentsDiv = document.querySelectorAll('.opponent')
     for (const opponentDiv of opponentsDiv) {
-      opponentDiv.onclick = this.opponentClicked.bind(this, opponentDiv.id)
+      opponentDiv.onclick = this.opponentClicked.bind(this, opponentDiv, opponentsDiv)
     }
   }
 }
